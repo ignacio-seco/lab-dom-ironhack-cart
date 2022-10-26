c94bfc67a8fafd4575738bd4b3ecd912428717e7
Ignácio Seco <itenorioseco@gmail.com>
2022-10-26 16:44:09 -0300

    done with the main interations, starting the bonus iterations

--- js/index.js
+++ js/index.js
@@ -1,26 +1,33 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
+
   console.log('Calculating subtotal, yey!');
 
   //... your code goes here
+const price = product.querySelector('.price span')
+console.log(price.innerText)
+const quantity = product.querySelector('.quantity input')
+console.log (quantity.value)
+const subtotal = product.querySelector('.subtotal span')
+let result = Number(price.innerText)*Number(quantity.value)
+console.log(result)
+subtotal.innerText=result
+return result
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  const multipleProducts = document.getElementsByClassName('product');
+  const totalValue= document.getElementById(`total-value`)
+  let totalResult=0
+  //console.log(multipleProducts)
+  for(let i=0;i<multipleProducts.length;i++){
+  totalResult+=updateSubtotal(multipleProducts[i])
 }
-
-// ITERATION 4
+{
+  totalValue.innerText=totalResult
+}
+}
 
 function removeProduct(event) {
   const target = event.currentTarget;
